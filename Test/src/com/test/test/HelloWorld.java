a7460a8ef3c7061e57b83bd8d93d24c8371dbb57
王笑笑 <15831664398>
2020-01-11 11:39:12 +0800

    这是测试

--- Test/src/com/test/test/HelloWorld.java
+++ Test/src/com/test/test/HelloWorld.java
@@ -1,5 +1,14 @@
 package com.test.test;
 
 public class HelloWorld {
+	
+	
+	
+	public static void main(String[] args) {
+	
+		System.out.println("这是测试哈哈哈哈");
+		System.err.println("这是测试");
+		
+	}
 
 }
